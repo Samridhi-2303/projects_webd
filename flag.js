68bef28ff1273f5faeb8cc88e1a8c68577c1d362
Samridhi-2303 <67220576+Samridhi-2303@users.noreply.github.com>
2020-06-29 22:20:55 +0530

    Add files via upload

---
+++ flag.js
@@ -0,0 +1,98 @@
+function brazilfunc(){
+  var c="This is flag of brazil";
+  document.getElementById("brazil").innerHTML=c;
+}
+function outBrazil(){
+    var c="  "
+    document.getElementById("brazil").innerHTML=c;
+}
+console.log("hello")
+function indiafunc(){
+  var c="This is flag of India";
+  document.getElementById("india").innerHTML=c;
+}
+function outindia(){
+    var c="  "
+    document.getElementById("india").innerHTML=c;
+}
+
+function argentinafunc(){
+  var c="This is flag of Argentina";
+  document.getElementById("argentina").innerHTML=c;
+}
+function outargentina(){
+    var c="  "
+    document.getElementById("argentina").innerHTML=c;
+}
+function englandfunc(){
+  var c="This is flag of England"; 
+  document.getElementById("england").innerHTML=c;
+}
+function outengland(){
+    var c="  "
+    document.getElementById("england").innerHTML=c;
+}
+function canadafunc(){
+  var c="This is flag of Canada"; 
+  document.getElementById("canada").innerHTML=c;
+}
+function outcanada(){
+    var c="  "
+    document.getElementById("canada").innerHTML=c;
+}
+function banglafunc(){
+  var c="This is flag of Bangladesh"; 
+  document.getElementById("bangladesh").innerHTML=c;
+}
+function outbangla(){
+    var c="  "
+    document.getElementById("bangladesh").innerHTML=c;
+}
+function srifunc(){
+  var c="This is flag of SriLanka"; 
+  document.getElementById("srilanka").innerHTML=c;
+}
+function outsri(){
+    var c="  "
+    document.getElementById("srilanka").innerHTML=c;
+}
+function malfunc(){
+  var c="This is flag of Malaysia"; 
+  document.getElementById("malaysia").innerHTML=c;
+}
+function outmal(){
+    var c="  "
+    document.getElementById("malaysia").innerHTML=c;
+}
+function singfunc(){
+  var c="This is flag of Singapore"; 
+  document.getElementById("singapore").innerHTML=c;
+}
+function outsing(){
+    var c="  "
+    document.getElementById("singapore").innerHTML=c;
+}
+function filifunc(){
+  var c="This is flag of Philippines"; 
+  document.getElementById("philippines").innerHTML=c;
+}
+function outfili(){
+    var c="  "
+    document.getElementById("philippines").innerHTML=c;
+}
+function japanfunc(){
+  var c="This is flag of Japan"; 
+  document.getElementById("japan").innerHTML=c;
+}
+function outjapan(){
+    var c="  "
+    document.getElementById("japan").innerHTML=c;
+}
+function koreafunc(){
+  var c="This is flag of Japan"; 
+  document.getElementById("skorea").innerHTML=c;
+}
+function outkorea(){
+    var c="  "
+    document.getElementById("skorea").innerHTML=c;
+}
